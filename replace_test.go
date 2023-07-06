672600333318158cae8e88fd3651d54342a7a8fe
Eizeiky <eizeiky@lidowin.com>
2023-07-07 00:26:19 +0700

    feat: add base64 to string function

--- replace_test.go
+++ replace_test.go
@@ -276,12 +276,11 @@ func TestFuncReplaceToUUID(t *testing.T) {
 	fmt.Println(init)
 }
 func TestFuncDecodeBase64ToStr(t *testing.T) {
-	trx_id := "b74bf098-b7ec-4593-80b9-e4194fbc12bf"
-	trx_id_base64 := "Yjc0YmYwOTgtYjdlYy00NTkzLTgwYjktZTQxOTRmYmMxMmJm"
+	str := "b74bf098-b7ec-4593-80b9-e4194fbc12bf"
+	base64 := "Yjc0YmYwOTgtYjdlYy00NTkzLTgwYjktZTQxOTRmYmMxMmJm"
 
-	dcd := funcDecodeBase64ToStr(trx_id_base64)
-
-	if dcd != trx_id {
+	base64ToStr := funcDecodeBase64ToStr(base64)
+	if base64ToStr != str {
 		t.Errorf("result from decode base64 not matched!")
 	}
 }
